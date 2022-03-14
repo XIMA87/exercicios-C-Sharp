6014f27076a43abfba132fa686da20a32b88f523
Pedro Luiz Braga <91227083+XIMA87@users.noreply.github.com>
2022-03-14 19:59:35 -0300

    added 2 new

---
+++ ExercicioAreaCirculo/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+O programa deve ler o valor do raio de um círculo e mostrar 
+na tela a área deste circulo com 4 casas decimais.
+*/
+
+using System;
+using System.Globalization;
+
+            double R, A, pi = 3.14159;
+
+            Console.Write("Digite o valor do raio: ");
+            R = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            A = pi * R * R;
+
+            Console.WriteLine("Área= " + A.ToString("F4", CultureInfo.InvariantCulture));
---
+++ ExercicioSoma/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//O programa deve ler 2 valores inteiros e mostrar na tela a soma deles.
+
+
+using System;
+
+            int A, B, soma;
+
+            Console.Write("Digite o primeiro número: ");
+            A = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            B = int.Parse(Console.ReadLine());
+
+            soma = A + B;
+        
+            Console.WriteLine("SOMA = " + soma);
